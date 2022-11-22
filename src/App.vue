326e49635a9e78fbc039ed2388f100f98795048f
yangjianwei <2017585616@qq.com>
2022-11-22 22:25:51 +0800

    购物车案例

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="app-container">
+        <Header :title="title"></Header>
+        <Goods v-for="goods in list" :key="goods.id" :goods="goods" @change_checked="change_checked"></Goods>
+        <Footer :list="list" @all_checked="all_checked"></Footer>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+
+import Header from '@/components/header/Header.vue'
+import Goods from '@/components/goods/Goods.vue'
+import Footer from '@/components/footer/Footer.vue'
+
+import bus from '@/eventBus.js'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            title: '购物车案例',
+            list: []
+        }
+    },
+
+    created() {
+        this.initGoods();
+    },
+
+    methods: {
+        async initGoods() {
+            var result = await axios.get("http://localhost:8888/initGoods")
+            /** 页面需要渲染的东西, 理论上都应该在 data 中定义! */
+            // console.log(result);
+            if (result.status === 200) {
+                this.list = result.data;
+            }
+        },
+
+        change_checked: function (obj) {
+            // console.log(obj);
+            var id = obj.goods_id;
+            var goods_state = obj.goods_state;
+            this.list.some((item, index) => {
+                if (id === item.id) {
+                    this.list[index].goods_state = goods_state;
+                    return true;
+                }
+            })
+        },
+
+        all_checked: function (checked) {
+            this.list.forEach((item, index) => {
+                this.list[index].goods_state = checked;
+            });
+        }
+    },
+
+    mounted() {
+        console.log('app.vue mounted');
+        var _this = this;
+        bus.$on('num_change', function(obj) {
+            var id = obj.goods_id;
+            var goods_count = obj.goods_count;
+            _this.list.some((item, index) => {
+                if (id === item.id) {
+                    _this.list[index].goods_count = goods_count;
+                    return true;
+                }
+            })
+        });
+    },
+
+    // 注册组件
+    components: {
+        Header,
+        Goods,
+        Footer
+    },
+
 }
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less" scoped>
+.app-container {
+    padding-top: 45px;
+    padding-bottom: 50px;
 }
 </style>
---
+++ src/components/counter/Counter.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="number-container d-flex justify-content-center align-items-center">
+        <!-- 减 1 的按钮 -->
+        <button @click="sub" type="button" class="btn btn-light btn-sm">-</button>
+        <!-- 购买的数量 -->
+        <span class="number-box">{{ goods.goods_count }}</span>
+        <!-- 加 1 的按钮 -->
+        <button @click="add" type="button" class="btn btn-light btn-sm">+</button>
+    </div>
+</template>
+  
+<script>
+import bus from '@/eventBus.js'
+
+export default {
+
+    props: {
+        goods: {
+            default: {},
+            type: Object
+        }
+    },
+
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+        sub() {
+            // console.log('counter.vue sub');
+            if (this.goods.goods_count <= 1) {
+                return;
+            } else {
+                var param = {
+                    goods_id: this.goods.id,
+                    goods_count: this.goods.goods_count - 1
+                };
+                bus.$emit('num_change', param);
+            }
+        },
+        
+        add() {
+            // console.log('counter.vue add');
+            var param = {
+                goods_id: this.goods.id,
+                goods_count: this.goods.goods_count + 1
+            };
+            console.log(param);
+            bus.$emit('num_change', param);
+        }
+    },
+};
+</script>
+
+
+<style lang="less" scoped>
+.number-box {
+    min-width: 30px;
+    text-align: center;
+    margin: 0 5px;
+    font-size: 12px;
+}
+
+.btn-sm {
+    width: 30px;
+}
+</style>
+
+
